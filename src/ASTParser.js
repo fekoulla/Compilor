d3cc62e8c11fe6c58d186cf119f88f35e7f6f8f9
Valentin <valou.pannacotta@gmail.com>
2018-03-22 14:51:10 +0100

    Added if statement

--- src/ASTParser.js
+++ src/ASTParser.js
@@ -2,49 +2,52 @@ import Expression from "./Expression.js";
 import ExpressionFactory from "./ExpressionFactory.js";
 
 export default class ASTParser{
-	constructor(){
-		this.ast = new Expression('Body');
-		this.last_exp= this.ast;
-	}
-	
-	static parse(tokens){
-		let instance = new ASTParser();
-		for(let cursor={position: 0}; cursor.position < tokens.length; cursor.position++){
-			let current_token = tokens[cursor.position];
-			switch(current_token.type){
-				case 'space':
-				case 'line-break-r':
-				case 'line-break':
-				case 'console-object':
-				case 'point':
-				case 'parenthesis-start':
-				case 'parenthesis-end':
-				case 'identifier':
-				case 'instruction-end':
+    constructor(){
+        this.ast = new Expression('Body');
+        this.last_exp= this.ast;
+    }
+
+    static parse(tokens){
+        let instance = new ASTParser();
+        for(let cursor={position: 0}; cursor.position < tokens.length; cursor.position++){
+            let current_token = tokens[cursor.position];
+            if(tokens.shift().type != 'script-php-start'){
+                throw 'You need to start your code with "<?php"'
+            }
+            switch(current_token.type){
+                case 'space':
+                case 'line-break-r':
+                case 'line-break':
+                case 'console-object':
+                case 'point':
+                case 'parenthesis-start':
+                case 'parenthesis-end':
+                case 'identifier':
+                case 'instruction-end':
                 case 'variable-declaration':
                 case 'operator':
-					continue;
-				default:
-					let exp = ExpressionFactory.create(cursor, tokens);
-					if(exp){
-						instance.addExpToTree(exp);
-					}else{
-						throw `grammar error`
-					}
-					break;
-			}
-		}
-		return instance.ast;
-	}
-	
-	addExpToTree(exp){
-		switch(exp.type){
-			case 'ExpressionVarDeclaration':
-			case 'ExpressionVarAssignation':
-				this.last_exp.addChild(exp);
-				break;
-		}
-	}
-	
-	
+                    continue;
+                default:
+                    let exp = ExpressionFactory.create(cursor, tokens);
+                    if(exp){
+                        instance.addExpToTree(exp);
+                    }else{
+                        throw `grammar error`
+                    }
+                    break;
+            }
+        }
+        return instance.ast;
+    }
+
+    addExpToTree(exp){
+        switch(exp.type){
+            case 'ExpressionVarDeclaration':
+            case 'ExpressionVarAssignation':
+                this.last_exp.addChild(exp);
+                break;
+        }
+    }
+
+
 }
--- src/ExpressionFactory.js
+++ src/ExpressionFactory.js
@@ -2,34 +2,45 @@ import ExpressionVarDeclaration from "./ExpressionVarDeclaration.js";
 import ExpressionVarAssignation from "./ExpressionVarAssignation.js";
 
 export default class ExpressionFactory{
-	
-	static create(cursor, tokens){
-		let current_token= tokens[cursor.position];
-		switch(current_token.type){
-			case 'variable-declaraction':
-				cursor.position++
-				let next = tokens[cursor.position];
-				if(next.type!="space"){
-					throw 'You have to put a space after a variable declaraction.';
-				}
-				cursor.position++
-				next = tokens[cursor.position];
-				return new ExpressionVarDeclaration(next);
-			case 'equal':
-				if(tokens[cursor.position-1].type=="space" && tokens[cursor.position-2].type=="identifier"){
-					cursor.position++
-					let next = tokens[cursor.position];
-					if(next.type!="space"){
-						throw 'You have to put a space after a variable assignation.';
-					}
-					cursor.position++
-					next = tokens[cursor.position];
-					return new ExpressionVarAssignation(tokens[cursor.position-4], next);	
-				}else{
-					throw 'You have to put a space after a variable declaraction.';
-				}
-				break;
-		}
-	}	
-	
+
+    static create(cursor, tokens){
+        let current_token= tokens[cursor.position];
+        console.log(current_token);
+        switch(current_token.type){
+            case 'variable-declaraction':
+                cursor.position++
+                let next = tokens[cursor.position];
+                if(next.type!="identifier"){
+                    throw 'You have to put an identifier after the $ at line ' + next.line;
+                }
+                cursor.position++
+                next = tokens[cursor.position];
+                return new ExpressionVarDeclaration(next);
+            case 'equal':
+                if(tokens[cursor.position-1].type=="space" && tokens[cursor.position-2].type=="identifier"){
+                    cursor.position++
+                    let next = tokens[cursor.position];
+                    if(next.type!="space"){
+                        throw 'You have to put a space after a variable assignation.';
+                    }
+                    cursor.position++
+                    next = tokens[cursor.position];
+                    return new ExpressionVarAssignation(tokens[cursor.position-4], next);	
+                }else{
+                    throw 'You have to put a space after a variable declaraction.';
+                }
+                break;
+                
+            case 'instruction-end':
+                cursor.position++
+                let next = tokens[cursor.position];
+                if(next.type!="identifier"){
+                    throw 'You have to put an identifier after the $ at line ' + next.line;
+                }
+                cursor.position++
+                next = tokens[cursor.position];
+                return new ExpressionVarDeclaration(next);
+        }
+    }	
+
 };
